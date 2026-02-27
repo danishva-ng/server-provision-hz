d223ba4277d9d19c21259f2dd6c06d816dab503e
Danish V A <danishva@Danishs-MacBook-Pro.local>
2026-02-27 14:47:45 +0400

    inital files for provosion file

--- providers.tf
+++ providers.tf
@@ -1,6 +1,22 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket                      = "tfpocserver"
+    key                         = "poc-k8s-bastion/terraform.tfstate"
+    region                      = "eu-central"   # dummy value required
+    endpoint                    = "https://nbg1.your-objectstorage.com"
+    
+
+    access_key                  = ""
+    secret_key                  = ""
+
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
+  }
+
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
